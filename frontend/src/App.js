3a98003937df709fb9de68d2dd4c439b48bc0628
Daniel Correa <yo@danielgara.com>
2021-06-23 14:16:02 -0500

    imp: chapter 14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() { 
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null){
+    // default user to null 
+    setUser(user) 
+  }
+
+  async function logout(){ 
+    setUser(null);
+  }
+
+  return ( 
+    <div className="App"> 
+      <Navbar bg="light" expand="lg"> 
+        <Navbar.Brand href="#home">
+          Movie Reviews
+        </Navbar.Brand> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+        <Navbar.Collapse id="basic-navbar-nav"> 
+          <Nav className="mr-auto">
+            <Nav.Link> 
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link> 
+              { user ? ( <a onClick={logout}>Logout User</a> ) : ( <Link to={"/login"}>Login</Link> )} 
+            </Nav.Link>
+          </Nav> 
+        </Navbar.Collapse> 
+      </Navbar>
+    </div> 
+  ); 
+} 
 
 export default App;
+ 
